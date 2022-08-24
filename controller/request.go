819e355afe58c0338e55f3ea90090bdb6a3ad249
fishworm96 <245525860@qq.com>
2022-08-24 20:11:59 +0800

    修改函数名

--- controller/request.go
+++ controller/request.go
@@ -10,8 +10,8 @@ const CtxUserIDKey = "userID"
 
 var ErrorUserNotLogin = errors.New("用户未登录")
 
-// getCurrentUser 获取当前登录的用户ID
-func getCurrentUser(c *gin.Context) (userID int64, err error) {
+// getCurrentUserID 获取当前登录的用户ID
+func getCurrentUserID(c *gin.Context) (userID int64, err error) {
 	uid, ok := c.Get(CtxUserIDKey)
 	if !ok {
 		err = ErrorUserNotLogin
