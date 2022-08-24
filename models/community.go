b28eac377e9a613f6cafec45c06484d565bd9087
fishworm96 <245525860@qq.com>
2022-08-24 19:56:46 +0800

    feat: 添加根据社区id获取列表

--- models/community.go
+++ models/community.go
@@ -1,6 +1,14 @@
 package models
 
+import "time"
+
 type Community struct {
-	Id int64 `json:"id" db:"community_id"`
+	Id   int64  `json:"id" db:"community_id"`
 	Name string `json:"name" db:"community_name"`
+}
+
+type CommunityDetail struct {
+	Community
+	Introduction string `json:"introduction,omitempty" db:"introduction"`
+	CreateTime   time.Time `json:"create_time" db:"create_time"`
 }
