d98340520a2429525925fe4a95784dfa1363d136
fishworm96 <245525860@qq.com>
2022-08-24 18:12:11 +0800

    feat：封装jwt校验

--- controller/code.go
+++ controller/code.go
@@ -5,9 +5,13 @@ type ResCode int64
 const (
 	CodeSuccess ResCode = 1000 + iota
 	CodeInvalidParam
+	CodeUserNotExist
 	CodeUserExist
 	CodeInvalidPassword
 	CodeServerBusy
+
+	CodeNeedLogin
+	CodeInvalidToken
 )
 
 var codeMsgMap = map[ResCode]string{
@@ -16,6 +20,8 @@ var codeMsgMap = map[ResCode]string{
 	CodeUserExist: "用户名已存在",
 	CodeInvalidPassword: "用户名或密码错误",
 	CodeServerBusy: "服务繁忙",
+	CodeNeedLogin: "需要登录",
+	CodeInvalidToken: "无效的token",
 }
 
 func (c ResCode) Msg() string {
