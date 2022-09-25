7dae3a03c4a83cec2dcb50d860e4ce7c8091ca8d
fishworm96 <245525860@qq.com>
2022-09-25 15:03:40 +0800

    fix: 修改错误信息

--- controller/code.go
+++ controller/code.go
@@ -22,6 +22,7 @@ const (
 var codeMsgMap = map[ResCode]string{
 	CodeSuccess: "success",
 	CodeInvalidParam: "请求参数错误",
+	CodeUserNotExist: "用户不存在",
 	CodeUserExist: "用户名已存在",
 	CodeInvalidPassword: "用户名或密码错误",
 	CodeServerBusy: "服务繁忙",
