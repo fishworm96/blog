d5184407085b667a8e8311fd59717b976ee9104c
fishworm96 <245525860@qq.com>
2022-10-06 00:03:42 +0800

    feat: 添加获取用户信息接口

--- models/user.go
+++ models/user.go
@@ -5,3 +5,15 @@ type User struct {
 	Username string `db:"username"`
 	Password string `db:"password"`
 }
+
+type Role struct {
+	Role_id int64 `json:"role_id" db:"role_id"`
+	Title string `json:"title" db:"title"`
+	Description string `json:"description" db:"description"`
+}
+
+type UserInfo struct {
+	Username string `json:"username"`
+	Title string `json:"title"`
+	Description string `json:"description"`
+}
