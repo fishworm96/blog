d40e3a5cc481925f0641304be2ef2c6de851bcdf
fishworm96 <245525860@qq.com>
2022-10-10 22:21:34 +0800

    feat: 添加添加菜单接口

--- models/menu.go
+++ models/menu.go
@@ -8,3 +8,11 @@ type MenuDetail struct {
 	Path string `json:"path" db:"path"`
 	Children []*MenuDetail `json:"children,omitempty"` // 过滤为空
 }
+
+type Menu struct {
+	Title string `json:"title" db:"title" binding:"required"`
+	Icon string `json:"icon" db:"icon"`
+	Path string `json:"path" db:"path" binding:"required"`
+	Type int64 `json:"type" db:"type" binding:"required"`
+	ModuleId int64 `json:"module_id" db:"module_id" binding:"required"`
+}
