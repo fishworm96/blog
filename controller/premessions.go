e78b522229288cbeaff86e408d8327d4abd0c39f
fishworm96 <245525860@qq.com>
2022-10-10 23:32:08 +0800

    feat: 添加更新菜单接口，修改参数模型位置

--- controller/premessions.go
+++ controller/premessions.go
@@ -9,6 +9,7 @@ import (
 	"go.uber.org/zap"
 )
 
+// 获取菜单列表
 func GetMenuHandler(c *gin.Context) {
 	data, err := logic.GetMenuList()
 	if err != nil {
@@ -19,8 +20,9 @@ func GetMenuHandler(c *gin.Context) {
 	ResponseSuccess(c, data)
 }
 
+// 添加菜单
 func AddMenuHandler(c *gin.Context) {
-	m := new(models.Menu)
+	m := new(models.ParamMenu)
 	if err := c.ShouldBindJSON(m); err != nil {
 		zap.L().Debug("c.ShouldBindJSON(m) failed", zap.Any("err", err))
 		zap.L().Error("add menu with invalid param")
@@ -37,6 +39,31 @@ func AddMenuHandler(c *gin.Context) {
 	if err != nil {
 		zap.L().Error("logic.AddMenuHandler(m) failed", zap.Error(err))
 		ResponseError(c, CodeAddFailed)
+		return
+	}
+
+	ResponseSuccess(c, nil)
+}
+
+// 修改菜单
+func UpdateMenuHandler(c *gin.Context) {
+	m := new(models.ParamUpdateMenu)
+	if err := c.ShouldBindJSON(m); err != nil {
+		zap.L().Debug("c.ShouldBindJSON(m) failed", zap.Any("err", err))
+		zap.L().Error("update menu with invalid param")
+		errs, ok := err.(validator.ValidationErrors)
+		if !ok {
+			ResponseError(c, CodeInvalidParam)
+			return
+		}
+		ResponseErrorWithMsg(c, CodeInvalidParam, removeTopStruct(errs.Translate(trans)))
+		return
+	}
+
+	err := logic.UpdateMenu(m)
+	if err != nil {
+		zap.L().Error("logic.UpdateMenu(m) failed", zap.Error(err))
+		ResponseError(c, CodeUpdateFailed)
 		return
 	}
 
--- routes/routes.go
+++ routes/routes.go
@@ -40,6 +40,7 @@ func Setup(mode string) *gin.Engine {
 	{
 		v1.GET("/menu", controller.GetMenuHandler)
 		v1.POST("/menu", controller.AddMenuHandler)
+		v1.PUT("/menu", controller.UpdateMenuHandler)
 	}
 	{
 		v1.POST("/post", controller.CreatePostHandler)
