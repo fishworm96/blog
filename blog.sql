3fef6c7a413936284c83ace4d9aad173572bf9b4
fishworm96 <245525860@qq.com>
2023-03-06 17:28:42 +0800

    fix: 修改数据库

--- blog.sql
+++ blog.sql
@@ -3,15 +3,15 @@
 
  Source Server         : localhost_3306
  Source Server Type    : MySQL
- Source Server Version : 80029
+ Source Server Version : 100417
  Source Host           : localhost:3306
  Source Schema         : blog
 
  Target Server Type    : MySQL
- Target Server Version : 80029
+ Target Server Version : 100417
  File Encoding         : 65001
 
- Date: 09/10/2022 23:10:11
+ Date: 06/03/2023 17:28:05
 */
 
 SET NAMES utf8mb4;
@@ -22,170 +22,132 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `access`;
 CREATE TABLE `access`  (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
   `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `type` tinyint NOT NULL DEFAULT 0,
+  `type` tinyint(4) NOT NULL DEFAULT 0,
   `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
-  `module_id` int NOT NULL DEFAULT 0,
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `module_id` int(11) NOT NULL DEFAULT 0,
+  `create_time` timestamp(0) NOT NULL DEFAULT current_timestamp(0),
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of access
--- ----------------------------
-INSERT INTO `access` VALUES (1, '首页', '0', 1, '/home/index', 0, '2022-09-29 22:40:09');
-INSERT INTO `access` VALUES (2, '权限管理', '0', 1, '/access', 0, '2022-10-06 22:43:08');
-INSERT INTO `access` VALUES (3, '菜单列表', '0', 2, '/access/menu', 2, '2022-10-06 22:49:11');
-INSERT INTO `access` VALUES (4, '所有菜单', '0', 3, '/access/menu/list', 3, '2022-10-09 22:44:18');
+) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for community
 -- ----------------------------
 DROP TABLE IF EXISTS `community`;
 CREATE TABLE `community`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `community_id` int UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `community_id` int(10) UNSIGNED NOT NULL,
   `community_name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `introduction` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_time` timestamp(0) NOT NULL DEFAULT current_timestamp(0),
+  `update_time` timestamp(0) NOT NULL DEFAULT current_timestamp(0) ON UPDATE CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `idx_community_id`(`community_id`) USING BTREE,
   UNIQUE INDEX `idx_community_name`(`community_name`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of community
--- ----------------------------
-INSERT INTO `community` VALUES (1, 1, 'Go', 'Golang', '2016-11-01 08:10:10', '2016-11-01 08:10:10');
-INSERT INTO `community` VALUES (2, 2, 'leetcode', '刷题刷题刷题', '2020-01-01 08:00:00', '2020-01-01 08:00:00');
-INSERT INTO `community` VALUES (3, 3, 'CS:GO', 'Rush B。。。', '2018-08-07 08:30:00', '2018-08-07 08:30:00');
-INSERT INTO `community` VALUES (4, 4, 'LOL', '欢迎来到英雄联盟!', '2016-01-01 08:00:00', '2016-01-01 08:00:00');
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for post
 -- ----------------------------
 DROP TABLE IF EXISTS `post`;
 CREATE TABLE `post`  (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `post_id` bigint NOT NULL COMMENT '帖子id',
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `post_id` bigint(20) NOT NULL COMMENT '帖子id',
   `title` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '标题',
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '描述',
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '内容',
-  `author_id` bigint NOT NULL COMMENT '作者的用户id',
-  `community_id` bigint NOT NULL COMMENT '所属社区',
-  `status` tinyint NOT NULL DEFAULT 1 COMMENT '帖子状态',
-  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `author_id` bigint(20) NOT NULL COMMENT '作者的用户id',
+  `community_id` bigint(20) NOT NULL COMMENT '所属社区',
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '帖子状态',
+  `create_time` timestamp(0) NULL DEFAULT current_timestamp(0) COMMENT '创建时间',
+  `update_time` timestamp(0) NULL DEFAULT current_timestamp(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `idx_post_id`(`post_id`) USING BTREE,
   INDEX `idx_author_id`(`author_id`) USING BTREE,
   INDEX `idx_community_id`(`community_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of post
--- ----------------------------
-INSERT INTO `post` VALUES (2, 20460448277069824, '打5把csgo', '打5把csgo', 18701801691811840, 3, 1, '2022-08-26 19:02:31', '2022-08-26 19:02:31');
-INSERT INTO `post` VALUES (3, 20852552107032576, '打5把csgo', '打5把csgo', 18701801691811840, 2, 1, '2022-08-27 21:00:35', '2022-08-27 21:00:35');
-INSERT INTO `post` VALUES (4, 20853674569891840, '发5把ak', '打6把csgo', 18701801691811840, 2, 1, '2022-08-27 21:05:03', '2022-08-29 23:47:23');
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for post_tag
 -- ----------------------------
 DROP TABLE IF EXISTS `post_tag`;
 CREATE TABLE `post_tag`  (
-  `post_id` bigint NULL DEFAULT NULL COMMENT '文章id',
+  `post_id` bigint(20) NULL DEFAULT NULL COMMENT '文章id',
   `tag_name` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '标签id'
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of post_tag
+-- Table structure for products
 -- ----------------------------
-INSERT INTO `post_tag` VALUES (20460448277069824, 'go');
-INSERT INTO `post_tag` VALUES (20460448277069824, 'javascript');
-INSERT INTO `post_tag` VALUES (20460448277069824, 'go');
-INSERT INTO `post_tag` VALUES (20460448277069824, 'go');
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) NULL DEFAULT NULL,
+  `updated_at` datetime(3) NULL DEFAULT NULL,
+  `deleted_at` datetime(3) NULL DEFAULT NULL,
+  `code` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_croatian_ci NULL,
+  `price` bigint(20) UNSIGNED NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_products_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_croatian_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for role
 -- ----------------------------
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `role_id` int NOT NULL DEFAULT 0,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) NOT NULL DEFAULT 0,
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_time` timestamp(0) NOT NULL DEFAULT current_timestamp(0),
+  `update_time` timestamp(0) NOT NULL DEFAULT current_timestamp(0),
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of role
--- ----------------------------
-INSERT INTO `role` VALUES (1, 1, '管理员', '管理员', '2022-09-29 22:38:54', '2022-09-29 22:38:54');
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for role_access
 -- ----------------------------
 DROP TABLE IF EXISTS `role_access`;
 CREATE TABLE `role_access`  (
-  `role_id` int NOT NULL,
-  `access_id` int NULL DEFAULT NULL,
-  PRIMARY KEY (`role_id`) USING BTREE
+  `role_id` int(11) NOT NULL,
+  `access_id` int(11) NOT NULL
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of role_access
--- ----------------------------
-INSERT INTO `role_access` VALUES (1, 1);
 
 -- ----------------------------
 -- Table structure for tag
 -- ----------------------------
 DROP TABLE IF EXISTS `tag`;
 CREATE TABLE `tag`  (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `tag_name` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '标签名称',
-  `created_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `updated_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `created_time` timestamp(0) NULL DEFAULT current_timestamp(0) COMMENT '创建时间',
+  `updated_time` timestamp(0) NULL DEFAULT current_timestamp(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '文章标签管理' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of tag
--- ----------------------------
-INSERT INTO `tag` VALUES (6, 'javascript', '2022-09-01 12:54:18', '2022-09-01 12:54:18');
-INSERT INTO `tag` VALUES (7, 'go', '2022-09-01 13:09:37', '2022-09-02 16:48:38');
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '文章标签管理' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `user_id` bigint NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
   `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `gender` tinyint NOT NULL DEFAULT 0,
-  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `is_super` tinyint NOT NULL COMMENT '1为超级管理员0为普通用户',
-  `role_id` int NOT NULL COMMENT '鉴权id',
+  `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '头像路径',
+  `gender` tinyint(4) NOT NULL DEFAULT 0,
+  `create_time` timestamp(0) NULL DEFAULT current_timestamp(0),
+  `update_time` timestamp(0) NULL DEFAULT current_timestamp(0) ON UPDATE CURRENT_TIMESTAMP(0),
+  `is_super` tinyint(4) NOT NULL DEFAULT 0 COMMENT '1为超级管理员0为普通用户',
+  `role_id` int(11) NOT NULL DEFAULT 0 COMMENT '鉴权id',
+  `nick_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '用户名称',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `idx_username`(`username`) USING BTREE,
   UNIQUE INDEX `idx_user_id`(`user_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES (1, 18701801691811840, '123', '313233827ccb0eea8a706c4c34a16891f84e7b', NULL, 0, '2022-08-21 22:34:16', '2022-09-29 22:13:59', 0, 0);
-INSERT INTO `user` VALUES (2, 18710686045573120, '123444', '313233343434827ccb0eea8a706c4c34a16891f84e7b', NULL, 0, '2022-08-21 23:09:35', '2022-09-29 22:13:58', 0, 0);
-INSERT INTO `user` VALUES (4, 21932215092903936, '123443', '313233343434827ccb0eea8a706c4c34a16891f84e7b', NULL, 0, '2022-08-30 20:30:47', '2022-09-29 22:13:58', 0, 0);
-INSERT INTO `user` VALUES (5, 30635805579935744, '123456', '$2a$10$0zuNzWuo.KHpHLDQqdcyAuR2jRWsofWug35VB1GFercORNCx1xl4a', NULL, 0, '2022-09-23 20:55:45', '2022-09-29 22:38:36', 1, 1);
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
