24e17b7048c952d47c601e405ed04fc831ff0f21
fishworm96 <245525860@qq.com>
2023-04-26 22:34:43 +0800

    feat: 忽略shell提交

--- bin/build.sh
+++
@@ -1,6 +0,0 @@
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o ./bin/blog
-ssh root@ "supervisorctl stop blog"
-scp ./bin/blog root@:/data/app/blog/bin/
-scp ./conf/config-docker.yaml root@:/data/app/blog/conf/config.yaml
-ssh root@ "supervisorctl start blog"
-rm ./bin/blog
